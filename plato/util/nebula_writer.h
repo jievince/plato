7ca7e8e5bb9c5c0a195d5c28e2b5539eff064369
jievince <38901892+jievince@users.noreply.github.com>
2021-06-29 14:44:16 +0800

    support type in nebula.conf and create tag if not exists

--- plato/util/nebula_writer.h
+++ plato/util/nebula_writer.h
@@ -22,14 +22,16 @@
 #ifndef __PLATO_UTIL_NEBULA_WRITER_HPP__
 #define __PLATO_UTIL_NEBULA_WRITER_HPP__
 
+#include <poll.h>
+
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
 #include <string>
 
-#include "glog/logging.h"
 #include "boost/algorithm/string.hpp"
 #include "boost/format.hpp"
+#include "glog/logging.h"
 #include "nebula/client/Config.h"
 #include "nebula/client/ConnectionPool.h"
 #include "nebula/client/Init.h"
@@ -39,7 +41,9 @@
 #include "thread_local_object.h"
 
 namespace std {
-inline string to_string(const std::string &__val) { return "\"" + __val + "\""; }
+inline string to_string(const std::string &__val) {
+  return "\"" + __val + "\"";
+}
 } // namespace std
 
 namespace plato {
@@ -53,14 +57,13 @@ static std::string mode_;
 static int retry_;
 static std::string err_file_;
 static std::string tag_;
-static std::vector<std::string> props_;
+static std::string prop_;
+static std::string type_;
 static size_t batch_size_;
 
 } // namespace nebula_writer_configs_detail
 
-std::string to_string(std::string value) {
-  return value;
-}
+std::string to_string(std::string value) { return value; }
 bool check_response(const nebula::ExecutionResponse &resp,
                     const std::string &stmt) {
   if (resp.errorCode != nebula::ErrorCode::SUCCEEDED) {
@@ -73,8 +76,7 @@ bool check_response(const nebula::ExecutionResponse &resp,
   return true;
 }
 
-template <typename ITEM>
-struct Buffer {
+template <typename ITEM> struct Buffer {
   size_t capacity_;
   nebula::Session *session_;
   std::function<int()> flush_;
@@ -125,12 +127,8 @@ struct Buffer {
     }
 
     if (boost::iequals(nebula_writer_configs_detail::mode_, "insert")) {
-      std::string stmt = "INSERT VERTEX " + nebula_writer_configs_detail::tag_ + "(";
-      for (auto &prop : nebula_writer_configs_detail::props_) {
-        stmt += prop;
-        stmt += ",";
-      }
-      stmt.back() = ')';
+      std::string stmt = "INSERT VERTEX " + nebula_writer_configs_detail::tag_ +
+                         "(" + nebula_writer_configs_detail::prop_ + ")";
       stmt += " VALUES ";
 
       for (auto &item : items_) {
@@ -148,12 +146,9 @@ struct Buffer {
         stmt += "UPDATE VERTEX ON " + nebula_writer_configs_detail::tag_ + " ";
         stmt += std::to_string(item.vid);
         stmt += " SET ";
-        for (auto &prop: nebula_writer_configs_detail::props_) {
-          stmt += prop;
-          stmt += " = ";
-          stmt += item.toString();
-          stmt += ",";
-        }
+        stmt += nebula_writer_configs_detail::prop_;
+        stmt += " = ";
+        stmt += item.toString();
         stmt.back() = ';';
       }
 
@@ -166,48 +161,51 @@ struct Buffer {
   };
 };
 
-template<typename ITEM>
-class thread_local_nebula_writer {
+template <typename ITEM> class thread_local_nebula_writer {
 public:
-  thread_local_nebula_writer(const thread_local_nebula_writer&) = delete;
-  thread_local_nebula_writer& operator=(const thread_local_nebula_writer&) = delete;
-  thread_local_nebula_writer(thread_local_nebula_writer&& x) noexcept : id_(x.id_) {
+  thread_local_nebula_writer(const thread_local_nebula_writer &) = delete;
+  thread_local_nebula_writer &
+  operator=(const thread_local_nebula_writer &) = delete;
+  thread_local_nebula_writer(thread_local_nebula_writer &&x) noexcept
+      : id_(x.id_) {
     x.id_ = -1;
   }
-  thread_local_nebula_writer& operator=(thread_local_nebula_writer &&x) noexcept {
+  thread_local_nebula_writer &
+  operator=(thread_local_nebula_writer &&x) noexcept {
     if (this != &x) {
       this->~thread_local_nebula_writer();
-      new(this) thread_local_nebula_writer(std::move(x));
+      new (this) thread_local_nebula_writer(std::move(x));
     }
     return *this;
   }
 
   thread_local_nebula_writer(const std::string &path) {
     Configs configs(path, "nebula:");
-    std::string graph_server_addrs, user, password, mode, space, tag, props, batch_size, retry, err_file;
+    std::string graph_server_addrs, user, password, mode, space, tag, prop,
+        type, batch_size, retry, err_file;
     CHECK((graph_server_addrs = configs.get("graph_server_addrs")) != "")
         << "graph_server_addrs doesn't exist.";
     CHECK((user = configs.get("user")) != "") << "user doesn't exist.";
     CHECK((password = configs.get("password")) != "")
         << "password doesn't exist.";
     CHECK((space = configs.get("space")) != "") << "space doesn't exist";
-    CHECK((mode = configs.get("mode")) != "") << "props doesn't exist.";
+    CHECK((mode = configs.get("mode")) != "") << "mode doesn't exist.";
     CHECK((tag = configs.get("tag")) != "") << "tag doesn't exist.";
-    CHECK((props = configs.get("props")) != "") << "props doesn't exist.";
-    CHECK((batch_size = configs.get("batch_size")) != "") << "batch_size doesn't exist.";
+    CHECK((prop = configs.get("prop")) != "") << "prop doesn't exist.";
+    CHECK((type = configs.get("type")) != "") << "type doesn't exist.";
+    CHECK((batch_size = configs.get("batch_size")) != "")
+        << "batch_size doesn't exist.";
     CHECK((retry = configs.get("retry")) != "") << "retry doesn't exist.";
-    CHECK((err_file = configs.get("err_file")) != "") << "err_file doesn't exist.";
+    CHECK((err_file = configs.get("err_file")) != "")
+        << "err_file doesn't exist.";
 
     std::vector<std::string> graphServers;
     boost::split(graphServers, graph_server_addrs, boost::is_any_of(","),
                  boost::token_compress_on);
 
-    std::vector<std::string> tagProps;
-    boost::split(tagProps, props, boost::is_any_of(","),
-                 boost::token_compress_on);
-
     auto batchSize = strtoul(batch_size.c_str(), nullptr, 10);
-    CHECK(batchSize > 0) << "batch_size: " << batchSize << " should be positive";
+    CHECK(batchSize > 0) << "batch_size: " << batchSize
+                         << " should be positive";
 
     auto &cluster_info = cluster_info_t::get_instance();
 
@@ -216,51 +214,59 @@ public:
     poolConfig.maxConnectionPoolSize_ = cluster_info.threads_;
     pool_->init(graphServers, poolConfig);
 
+    auto session = pool_->getSession(user, password);
+    CHECK(session.valid()) << "session is not valid";
+    auto stmt = "USE " + space + ";CREATE TAG IF NOT EXISTS " + tag + "(" + prop + " " + type + ")";
+    auto result = session.execute(stmt);
+    CHECK(check_response(result, stmt));
+    poll(nullptr, 0, 10000); // wait for schema to load into cache
+
     nebula_writer_configs_detail::user_ = user;
     nebula_writer_configs_detail::password_ = password;
     nebula_writer_configs_detail::space_ = space;
     nebula_writer_configs_detail::mode_ = mode;
     nebula_writer_configs_detail::tag_ = tag;
-    nebula_writer_configs_detail::props_ = tagProps;
+    nebula_writer_configs_detail::prop_ = prop;
+    nebula_writer_configs_detail::type_ = type;
     nebula_writer_configs_detail::batch_size_ = batchSize;
     nebula_writer_configs_detail::retry_ = stoi(retry);
     nebula_writer_configs_detail::err_file_ = err_file;
 
-    std::function<void *()> construction(
-        [this] {
-          nebula::Session *session =
-              new nebula::Session(this->pool_->getSession(nebula_writer_configs_detail::user_, nebula_writer_configs_detail::password_));
-          CHECK(session) << "session is nullptr";
-          CHECK(session->valid()) << "session is not valid";
-          CHECK(session->ping()) << "session ping failed";
-          std::string stmt;
-          nebula::ExecutionResponse result;
+    std::function<void *()> construction([this] {
+      nebula::Session *session = new nebula::Session(
+          this->pool_->getSession(nebula_writer_configs_detail::user_,
+                                  nebula_writer_configs_detail::password_));
+      CHECK(session->valid()) << "session is not valid";
+      CHECK(session->ping()) << "session ping failed";
+      std::string stmt;
+      nebula::ExecutionResponse result;
 
-          stmt = "USE " + nebula_writer_configs_detail::space_;
-          result = session->execute(stmt);
-          CHECK(check_response(result, stmt));
+      stmt = "USE " + nebula_writer_configs_detail::space_;
+      result = session->execute(stmt);
+      CHECK(check_response(result, stmt));
 
-          stmt = "DESC TAG " + nebula_writer_configs_detail::tag_;
-          result = session->execute(stmt);
-          CHECK(check_response(result, stmt));
+      auto *buff_ =
+          new Buffer<ITEM>(nebula_writer_configs_detail::batch_size_, session);
 
-          auto *buff_ = new Buffer<ITEM>(nebula_writer_configs_detail::batch_size_, session);
-
-          return (void *)buff_;
-        });
+      return (void *)buff_;
+    });
 
     std::function<void(void *)> destruction([](void *p) {
       auto *buff_ = (Buffer<ITEM> *)p;
       if (!buff_->items_.empty()) {
-        LOG(INFO) << "flush_ in desctruction, items_.size()=" << buff_->items_.size();
+        LOG(INFO) << "flush_ in desctruction, items_.size()="
+                  << buff_->items_.size();
         buff_->flush_();
       }
       delete buff_;
     });
 
-    id_ = thread_local_object_detail::create_object(std::move(construction), std::move(destruction));
+    id_ = thread_local_object_detail::create_object(std::move(construction),
+                                                    std::move(destruction));
     LOG(INFO) << "thread_local_object_detail::create_object, id_: " << id_;
-    if (-1 == id_) throw std::runtime_error("thread_local_object_detail::create_object failed.");
+    if (-1 == id_)
+      throw std::runtime_error(
+          "thread_local_object_detail::create_object failed.");
   }
 
   ~thread_local_nebula_writer() {
@@ -276,10 +282,11 @@ public:
     LOG(INFO) << "finish call ~thread_local_nebula_writer";
   }
 
-  void foreach(std::function<void(const std::string& filename, boost::iostreams::filtering_ostream& os)> reducer) {};
+  void foreach (std::function<void(const std::string &filename,
+                                   boost::iostreams::filtering_ostream &os)>
+                    reducer){};
 
-  [[gnu::always_inline]] [[gnu::hot]]
-  Buffer<ITEM> &local() {
+  [[gnu::always_inline]] [[gnu::hot]] Buffer<ITEM> &local() {
     return *((Buffer<ITEM> *)thread_local_object_detail::get_local_object(id_));
   }
 
